57bb84a99d34232577b008f14d6282452cbdd426
Harith Iqbal <harith.iqbal@swiftlogistics.com.my>
2025-07-25 17:16:40 +0800

    chore: clean up

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
-import { R2Explorer } from "r2-explorer";
+import { R2Explorer } from 'r2-explorer'
 
-export default R2Explorer({
-  // Set to false to allow users to upload files
-  readonly: true,
-
-  // Learn more how to secure your R2 Explorer instance:
-  // https://r2explorer.com/getting-started/security/
-  // cfAccessTeamName: "my-team-name",
-});
+export default {
+  async fetch(request: Request, env: Env, ctx: ExecutionContext) {
+    return R2Explorer({
+      readonly: false,
+      basicAuth: [
+        {
+          username: 'mishu',
+          // @ts-expect-error
+          password: env.EXP_PW
+        }
+      ]
+      // @ts-expect-error
+    }).fetch(request, env, ctx)
+  }
+}
